d5b21363f8d68925e25d6757bb709ed89512a6b2
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2016-10-22 16:30:20 +0530

    Change Copyright year and add Names

--- src/traffic-control/examples/red-vs-nlred.cc
+++ src/traffic-control/examples/red-vs-nlred.cc
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2015 NITK Surathkal
+ * Copyright (c) 2016 NITK Surathkal
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,7 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ * Author: Phani Kiran <>
+ *         Nichit <>
+ *         Mohit P. Tahiliani <tahiliani@nitk.edu.in>
  *
  */
 
