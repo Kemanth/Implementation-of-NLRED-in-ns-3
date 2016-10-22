8cdebfd2953e49c89e92a3fa5dc77e90132af49e
Mohit P. Tahiliani <tahiliani@nitk.edu.in>
2016-10-22 16:31:38 +0530

    Authors instead of Author

--- src/traffic-control/examples/red-vs-nlred.cc
+++ src/traffic-control/examples/red-vs-nlred.cc
@@ -15,9 +15,9 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * Author: Phani Kiran <>
- *         Nichit <>
- *         Mohit P. Tahiliani <tahiliani@nitk.edu.in>
+ * Authors: Phani Kiran <>
+ *          Nichit <>
+ *          Mohit P. Tahiliani <tahiliani@nitk.edu.in>
  *
  */
 
